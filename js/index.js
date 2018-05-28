5b796110b3a42ef6f026f3d05e285e9b295d6761
wojtii <wojdna@gmail.com>
2018-05-28 12:16:06 +0200

    fixed formatting

--- js/index.js
+++ js/index.js
@@ -21,8 +21,6 @@ firebase.on('value', function(snapshot) {
   drawGraph(temp, hum, light, motion);
 });
 
-
-
 function drawGraph(temp, hum, light, motion){
   var labels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
   var ctx = document.getElementById("myChart").getContext('2d');
@@ -98,5 +96,4 @@ var myChart = new Chart(ctx, {
                 }
             }
 });
-
 }
